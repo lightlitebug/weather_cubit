ac17c08760aac2ed77e0b43768fa9f45fb34960c
lightlitebug <light.lite.bug@gmail.com>
2022-02-15 15:29:39 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather_cubit
+
+- Cubit + StreamSubscription
---
+++ lib/cubits/theme/theme_state.dart
@@ -0,0 +1,31 @@
+part of 'theme_cubit.dart';
+
+enum AppTheme {
+  light,
+  dark,
+}
+
+class ThemeState extends Equatable {
+  final AppTheme appTheme;
+  ThemeState({
+    this.appTheme = AppTheme.light,
+  });
+
+  factory ThemeState.initial() {
+    return ThemeState();
+  }
+
+  @override
+  List<Object> get props => [appTheme];
+
+  @override
+  bool get stringify => true;
+
+  ThemeState copyWith({
+    AppTheme? appTheme,
+  }) {
+    return ThemeState(
+      appTheme: appTheme ?? this.appTheme,
+    );
+  }
+}
